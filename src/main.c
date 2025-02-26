2b820fa84c340a3c23510c11d05bbb0c9691f4de
UnknownPgr <unknownpgr@gmail.com>
2025-02-26 19:28:46 +0900

    Implement simple knob control

--- src/main.c
+++ src/main.c
@@ -34,21 +34,53 @@ int main()
     motor_enable(true);
     print("Motor enabled");
 
+    float gain_p = 0.002;
+    float gain_i = 0.00002;
+    float gain_d = 0.01;
+
+    int32_t div = 1024;
+
+    float max_speed = 0.2;
+    float error_accum = 0;
+    float error_prev = 0;
     uint32_t target_position = 0;
     uint32_t current_position = 0;
-    float gain = 0.0001;
+    uint8_t index = 0;
 
     while (true)
     {
         encoder_update();
         encoder_get_counts(&current_position, &current_position);
 
-        int32_t diff = target_position - current_position;
-        float speed = -gain * diff;
-        if (speed > 1)
-            speed = 1;
-        if (speed < -1)
-            speed = -1;
+        int32_t error = target_position - current_position;
+
+        if (error > (div / 2))
+        {
+            target_position -= div;
+            index--;
+            print_index(index);
+        }
+        else if (error < -(div / 2))
+        {
+            target_position += div;
+            index++;
+            print_index(index);
+        }
+
+        error_accum += error;
+        if (error_accum > 1000)
+            error_accum = 1000;
+        if (error_accum < -1000)
+            error_accum = -1000;
+
+        float error_diff = error - error_prev;
+        error_prev = error;
+
+        float speed = -(gain_p * error + gain_i * error_accum + gain_d * error_diff);
+        if (speed > max_speed)
+            speed = max_speed;
+        if (speed < -max_speed)
+            speed = -max_speed;
         motor_set_velocity(0, speed);
     }
 
