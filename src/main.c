19817455ac75a033da09a2bdc6a6f8e4c51001b4
UnknownPgr <unknownpgr@gmail.com>
2025-02-27 11:35:18 +0900

    Test timer

--- src/main.c
+++ src/main.c
@@ -1,12 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <signal.h>
+#include <math.h>
+
+#include <log.h>
 #include <dev.h>
 #include <motor.h>
-#include <log.h>
-#include <math.h>
 #include <encoder.h>
-#include <signal.h>
+#include <timer.h>
 
 void signal_handler()
 {
@@ -26,13 +28,13 @@ void signal_handler()
     exit(0);
 }
 
-void set_signal_handler()
+void init()
 {
+    // Configure signal handler
     struct sigaction action;
     action.sa_handler = signal_handler;
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
-
     sigaction(SIGHUP, &action, NULL);
     sigaction(SIGINT, &action, NULL);
     sigaction(SIGQUIT, &action, NULL);
@@ -42,102 +44,58 @@ void set_signal_handler()
     sigaction(SIGSEGV, &action, NULL);
     sigaction(SIGPIPE, &action, NULL);
     sigaction(SIGTERM, &action, NULL);
-}
 
-void print_index(uint8_t index)
-{
-    index = index % 16;
-    char buffer[17];
-    for (uint8_t i = 0; i < 16; i++)
+    // Initialize peripherals
+    if (!dev_init())
     {
-        if (i == index)
-            buffer[i] = 'X';
-        else
-            buffer[i] = '-';
+        print("Failed to initialize device");
+        exit(1);
     }
-    buffer[16] = '\0';
-    printf("Index: %02d %s\r", index, buffer);
-    fflush(stdout);
+    print("Device initialized");
+
+    // Initialize timer
+    if (!timer_init())
+    {
+        print("Failed to initialize timer");
+        exit(1);
+    }
+    print("Timer initialized");
+
+    // Initialize motor
+    if (!motor_init())
+    {
+        print("Failed to initialize motor");
+        exit(1);
+    }
+    print("Motor initialized");
+
+    // Initialize encoder
+    if (!encoder_init())
+    {
+        print("Failed to initialize encoder");
+        exit(1);
+    }
+    print("Encoder initialized");
 }
 
 int main()
 {
-    set_signal_handler();
     print("Program started");
 
-    if (!dev_init())
+    // Initialization
+    init();
+
+    uint32_t start_ns = timer_get_ns();
+    uint32_t ns;
+    uint64_t counter = 0;
+    while ((uint32_t)((ns = timer_get_ns()) - start_ns) % TIMER_NS_MAX < 100000000)
     {
-        print("Failed to initialize device");
-        return 1;
+        counter++;
     }
-    print("Device initialized");
 
-    if (!motor_init())
-    {
-        print("Failed to initialize motor");
-        return 1;
-    }
-    print("Motor initialized");
-
-    if (!encoder_init())
-    {
-        print("Failed to initialize encoder");
-        return 1;
-    }
-    print("Encoder initialized");
-
-    motor_enable(true);
-    print("Motor enabled");
-
-    float gain_p = 0.002;
-    float gain_i = 0.00002;
-    float gain_d = 0.01;
-
-    int32_t div = 1024;
-
-    float max_speed = 0.2;
-    float error_accum = 0;
-    float error_prev = 0;
-    uint32_t target_position = 0;
-    uint32_t current_position = 0;
-    uint8_t index = 0;
-
-    while (true)
-    {
-        encoder_update();
-        encoder_get_counts(&current_position, &current_position);
-
-        int32_t error = target_position - current_position;
-
-        if (error > (div / 2))
-        {
-            target_position -= div;
-            index--;
-            print_index(index);
-        }
-        else if (error < -(div / 2))
-        {
-            target_position += div;
-            index++;
-            print_index(index);
-        }
-
-        error_accum += error;
-        if (error_accum > 1000)
-            error_accum = 1000;
-        if (error_accum < -1000)
-            error_accum = -1000;
-
-        float error_diff = error - error_prev;
-        error_prev = error;
-
-        float speed = -(gain_p * error + gain_i * error_accum + gain_d * error_diff);
-        if (speed > max_speed)
-            speed = max_speed;
-        if (speed < -max_speed)
-            speed = -max_speed;
-        motor_set_velocity(0, speed);
-    }
+    print("Counter: %d", counter);
+    print("Time: %lld", ns - start_ns);
+    print("Frequency: %fMHz", (float)counter / ((float)(ns - start_ns) / 1000.0f));
 
     return 0;
 }
