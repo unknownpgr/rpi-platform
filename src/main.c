a39ed5e446c864250cd0271770efaa70e3761936
UnknownPgr <unknownpgr@gmail.com>
2025-03-24 23:37:13 +0900

    Implement direction locking

--- src/main.c
+++ src/main.c
@@ -5,9 +5,11 @@
 
 #include <log.h>
 #include <timer.h>
+#include <motor.h>
+
 #include <imu-service.h>
 
-uint32_t dt()
+uint32_t dt_ns()
 {
     static bool is_initialized = false;
     static uint32_t last_time = 0;
@@ -15,7 +17,7 @@ uint32_t dt()
     if (is_initialized)
     {
         uint32_t current_time = timer_get_ns();
-        uint32_t delta_time = current_time - last_time;
+        uint32_t delta_time = DIFF(current_time, last_time);
         last_time = current_time;
         return delta_time;
     }
@@ -31,16 +33,29 @@ int application_start()
 {
     print("Program started");
     imu_init();
+    motor_init();
+    motor_enable(true);
+
+    const double Kp = 2.0;
+    const double Ki = 15.0;
+    const double Kd = 0.02;
+
+    imu_data_t data;
+    double angle = 0;
+    double integral = 0;
 
     while (true)
     {
-        imu_data_t data;
-
         imu_read(&data);
+        double dt = dt_ns() / 1e9;
 
-        print("ax: %d, ay: %d, az: %d, gx: %d, gy: %d, gz: %d", data.ax, data.ay, data.az, data.gx, data.gy, data.gz);
+        double angularVelocity = data.gz * 0.01;
+        angle += angularVelocity * dt;
+        integral += angle * dt;
 
-        timer_sleep_ns(10000000);
+        double output = Kp * angle + Ki * integral + Kd * angularVelocity;
+        motor_set_velocity(output, output);
+        timer_sleep_ns(1000000); // 1ms
     }
 
     return 0;
